4417dd0f231b4717b8b069ad784253fdfecc7243
hevinamin <aminhevin7@gmail.com>
2025-06-11 15:26:44 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ function calculatetotal() {
     document.getElementById("totalatend").innerText = 
     "Your Future Holdings: $" + totalatend.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
 }
-function updateRateValue(val) {
-    document.getElementById("rateCounter").innerText = val;}
+
 function updateYearsValue(val) {
     document.getElementById("yearsCounter").innerText = val;}
